7b4fd87b42d1e69c2094291dc948f7170c8419e4
Alejandro <a.perera0118@gmail.com>
2021-06-15 17:31:28 -0400

    added script to add data into database

--- person/apps.py
+++ person/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class PersonConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'person'
+    
+    def ready(self):
+        """Call the ready method and connect receiver when database is connected """
+        super(PersonConfig, self).ready()
+        import person.signals
+
